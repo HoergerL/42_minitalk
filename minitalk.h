87183ba547c058bd7e20f0b2ba9b08fc39bf1012
Laura Hoerger <lhoerger@1-D-7.42heilbronn.de>
2021-10-13 13:57:44 +0200

    added header

---
+++ minitalk.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lhoerger <lhoerger@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/13 13:33:18 by lhoerger          #+#    #+#             */
+/*   Updated: 2021/10/13 13:38:03 by lhoerger         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <signal.h>
+# include <stdlib.h>
+# include "libft/libft.h"
+# include "ft_printf/ft_printf.h"
+
+#endif
